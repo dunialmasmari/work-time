d38548ff55530ef2b70a46f3c7e65aa65a7f2c45
HaifaaNabeel <hauof53@gmail.com>
2021-02-14 12:11:47 +0300

    translate

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -8,7 +8,7 @@
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
 
         <div class="info">
-          <a href="#" class="d-block">Admin</a>
+          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
         </div>
       </div>
 
